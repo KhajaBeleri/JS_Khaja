752abe8a7b2755545502e4147768a3554d2053d2
KhajaBeleri <144865546+KhajaBeleri@users.noreply.github.com>
2024-07-23 17:39:53 +0000

    Convsersion of data types

---
+++ JS_01_Basics/JS_03_ConversionOperation.js
@@ -0,0 +1,35 @@
+//let score = "33abc";
+//let score = null;
+//let score = undefined;
+//let score = true;
+let score = "khaja"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isloggedIn="khaja";
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "khaja" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof stringNumber);
+
