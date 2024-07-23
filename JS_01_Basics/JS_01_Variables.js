6851575cd4ad596720a94bd31495f7263a406003
KhajaBeleri <144865546+KhajaBeleri@users.noreply.github.com>
2024-07-23 17:17:37 +0000

    variables and data types

---
+++ JS_01_Basics/JS_01_Variables.js
@@ -0,0 +1,27 @@
+const account_Id = 1404 
+let account_Email = "krbeleri@gmail.com"
+var account_Password = "12345"
+account_City = "Gadag"
+let account_State;
+
+//account_Id=2 //not allowed 
+
+account_Email="khaja@google.com"
+account_Password="9538371734"
+account_City="Bangaluru"
+
+console.log(account_Id);
+
+/*var
+{
+
+}
+*/
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([account_Id,account_Email,account_Password,account_City,account_State])
