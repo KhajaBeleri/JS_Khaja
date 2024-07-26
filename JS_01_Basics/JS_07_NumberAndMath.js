609491cfe1036e3f109cd23f112583b4d4117766
KhajaBeleri <144865546+KhajaBeleri@users.noreply.github.com>
2024-07-26 04:04:35 +0000

    Number and Math

---
+++ JS_01_Basics/JS_07_NumberAndMath.js
@@ -0,0 +1,35 @@
+const score = 400;
+//console.log(score);
+
+const bal= new Number(100);
+//console.log(bal);
+
+//console.log(bal.toString().length);
+//console.log(bal.toFixed(2));
+
+const otherNumber = 123.8966;
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds= 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,2,9,4));
+// console.log(Math.max(2,0,8,10));
+
+console.log(Math.random());// between 0 and 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min); 
